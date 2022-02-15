b86f19f2218fdc64828114b61dc2e976c49fdeea
asima <asima.chowdhury@sjinnovation.com>
2022-02-15 17:48:32 +0600

    added concurrently

--- src/components/CodeForInterview.jsx
+++ src/components/CodeForInterview.jsx
@@ -7,15 +7,18 @@ const useStyles = makeStyles({
     image: {
         width: '50%',
         height: '50%'
+    },
+    component: {
+        margin: 50
     }
 })
 
 const CodeForInterview = () => {
-    const classes = useStyles(); 
+    const classes = useStyles();
 
     return (
-        <Box>
-            <Typography variant='h4'> Code For Interview </Typography>
+        <Box className={classes.component}>
+            <Typography variant='h4' style={{marginBottom: 50}}> Code For Interview </Typography>
             <Box style={{ display: 'flex' }}>
                 <img className={classes.image} src={YouTube} alt="YouTube" />
                 <img className={classes.image} src={InstaTele} alt="InstaTele" />
