0ea1ab4a864f7ea3887890ec4db458d06ae814fc
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-02-20 07:36:20 +0600

    completed allusers dynamically read only table from json-server

--- src/Service/api.js
+++ src/Service/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const url = "http://localhost:3003/users";
 
-export const getUsers = () => {
-    return axios.get(url);
+export const getUsers = async () => {
+    return await axios.get(url);
 }
--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUsers } from '../Service/api';
+import { Table, TableHead, TableRow, TableCell, TableBody , makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    table:{
+        width: '90%',
+        margin: '50px 0 0 50px',
+    },
+    thead:{
+        '& > *':{
+            background: '#000000',
+            color: '#ffffff',
+            fontSize: 20,
+        }
+    },
+    row:{
+        '& > *':{
+            fontSize: 20,
+        }
+    }
+});
 
 const AllUsers = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const classes = useStyles();
+
+    useEffect(() => {
+        getAllUsers();
+    }, [])
+
+    const getAllUsers = async () => {
+        const response = await getUsers();
+        console.log(response.data);
+        setUsers(response.data);
+    }
+
     return (
         <div>
-            <h1>all users</h1>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow className={classes.thead}>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Username</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Phone</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        users.map(user => (
+                            <TableRow key={user.id} className={classes.row}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.username}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{user.phone}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
         </div>
     );
 };
